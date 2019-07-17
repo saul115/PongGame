72d3a3d68be4f532a7ee9cdb217e4ca3f4a065f5
Saul Augusto Gasca Farrera <sgfarrera@gmail.com>
2019-07-17 15:54:36 -0500

    Added Behaviour to The Ball with scenes collisions and Created The Ball Script
    I used RigidBody 2D and Circle Collider

---
+++ AndroidPongGame/Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+
+
+    public Transform paddle;
+    bool gameStarted = false;
+
+    public Rigidbody2D rbBall;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!gameStarted)
+        {
+
+            float positionDifference = paddle.position.x - transform.position.x;
+            transform.position = new Vector3(paddle.position.x - positionDifference, paddle.position.y, paddle.position.z);
+
+            if(Input.GetMouseButtonDown(0))
+            {
+                rbBall.velocity = new Vector2(8, 8);
+                gameStarted = true;
+            }
+
+        }
+
+    }
+}
--- AndroidPongGame/Assets/Scripts/PlayerMovement.cs
+++ AndroidPongGame/Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,37 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    //public GameObject paddle;
+    //public GameObject ball;
+   
+
+
     void Start()
     {
-        
+       // paddle = GameObject.Find("Paddle");
+        //ball = GameObject.Find("Ball");
+
+      
+        //transform.position = new Vector3(-7.74f,-0.07f,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Input.mousePosition);
 
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        transform.position = new Vector3(transform.position.x, Mathf.Clamp(mousePosition.y,-3.8f,3.8f), transform.position.z);
+          
+
+            Debug.Log(Input.mousePosition);
+
+             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+             transform.position = new Vector3(transform.position.x, Mathf.Clamp(mousePosition.y, -3.8f, 3.8f), transform.position.z);
+
+
+   
+
+        
     }
 }
